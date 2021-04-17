0a0b5ddfb37e0c2e5cbba8aac7b8cd31ded4c270
gabe thomas <gabe.thomas@bizzabo.com>
2021-04-17 13:40:55 -0400

    refactored index.js from mistake earlier

--- index.js
+++ index.js
@@ -16,147 +16,140 @@ const htmlAsync = util.promisify(fs.writeFile);
 function startPrompt() {
     inquirer.prompt([
         {
-            name: "teamname",
-            message: "Pick a name for your team"
+            name: "teamName",
+            type: "list",
+            message: "What team member are you adding?",
+            choices: ['Manager', 'Engineer', 'Intern', 'No more! there is too many']
         }
-    ])
-        .then(function(data){
-            const myTeamName = data.teamname;
-            myTeamArray.push(myTeamName)
-            addManager();
-        })
-}
+    ]).then(myChoice => {
+        switch(myChoice.teamName) {
+            case 'Manager':
+                managerQuestions();
+                break;
+            case 'Engineer':
+                engineerQuestions();
+                break;
+            case 'Intern':
+                internQuestions();
+                break;
+            case 'No more! there is too many':
+                print();
+                break;
+        }
+    });
+};
 
-function addMyManager() {
+function managerQuestions() {
     inquirer.prompt([
         {
-            name: "name",
-            message: "what is your manager's name?"
+            name: "managerName",
+            type: "input",
+            message: "What's the manager's name again?"
         },
-        
+
         {
-            name: "email",
-            message: "what is your manager's email?"
+            name: "managerId",
+            type: "input",
+            message: "What's the manager's ID?"
+        },
+
+        {
+            name: "managerEmail",
+            type: "input",
+            message: "What is your Manager's email?",
         },
 
         {
             name: "officeNumber",
-            type: "number",
-            message: "what is your managers office number?"
+            type: "input",
+            message: "What's your managers office number"
         },
-    ])
+    ]).then((myInput) => {
+        var manager = new Manager (myInput.managerName, myInput.managerId, myInput.managerEmail, myInput.officeNumber);
+        myTeamArray.push(manager);
+        startPrompt();
+    })
+};
 
-    .then(function(data) {
-        const name = data.name;
-        const id = 1;
-        const email = data.email;
-        const officeNumber = data.officeNumber;
-        const teamMember = new Manager(name, id, email, officeNumber)
-        myTeamArray.push(teamMember);
-        addMyTeam();
-    });
-}
-
-function addMyTeam() {
+function engineerQuestions() {
     inquirer.prompt([
         {
-            name: "memberData",
-            type: "list",
-            message: "add some peeps to your team!",
-            choices: ["Add an Engineer!", "Add an Intern!", "No more! There's just too many!"]
-        }
-    ])
-        .then(function(data){
-            switch(data.memberData) {
-                case "Add an Engineer!":
-                    addEngineer();
-                    break;
-                case "Add an Intern!":
-                    addIntern();
-                    break;
-                case "No more! There's just too many!":
-                    completedTeam();
-                    break
-            }
-        });
-}
+            name: "engineerName",
+            type: "input",
+            message: "What's your engineer's name?"
+        },
 
-function addEngineer() {
+        {
+            name: "engineerId",
+            type: "input",
+            message: "What's your Engineer's id number?"
+        },
+
+        {
+            name: "engineerEmail",
+            type: "input",
+            message: "What's your Engineer's email address?"
+        },
+
+        {
+            name: "engineerGithub",
+            type: "input",
+            message: "What is the team Engineer's GitHub?",
+        },
+
+    ]).then((myInput) => {
+        var engineer = new Engineer (myInput.engineerName, myInput.engineerId, myInput.engineerEmail, myInput.engineerGithub);
+        myTeamArray.push(engineer);
+        startPrompt();
+    })
+};
+
+function askInternQuestion() {
     inquirer.prompt([
-        {
-            name: "name",
-            message: "What's your new engineer's name?"
-        },
+      {
+            name: "internName",
+            type: "input",
+            message: "What's your intern's name?"
+      },
 
-        {
-            name: "email",
-            message: "What's this new engineer's email address?"
-        },
+      {
+            name: "internId",
+            type: "input",
+            message: "What's your intern's id number?"
+      },
 
-        {
-            name: "github",
-            message: "What's this engineer's Github username?"
-        }
-    ])
+      {
+            name: "internEmail",
+            type: "input",
+            message: "What's your intern's email?",
+      },
 
-        .then(function(data) {
-            const name = data.name;
-            const id = myTeamArray.length + 1;
-            const email = data.email;
-            const github = data.github
-            const teamMember = new Engineer(name, id, email, github)
-            myTeamArray.push(teamMember)
-            addMyTeam();
-        });
-}
+      {
+            name: "internSchool",
+            type: "input",
+            message: "What school is your intern currently attending?"
+      },
 
-function addIntern() {
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "What's the new intern's name?"
-
-        },
-
-        {
-            name: "email",
-            message: "What's this new intern's email address?"
-
-        },
-
-        {
-            name: "school",
-            message: "I forget, where did this new intern go to school?"
-
-        }
-    ])
-
-        .then(function (data) {
-            const name = data.name
-            const id = finalTeamArray.length + 1
-            const email = data.email
-            const school = data.school
-            const teamMember = new Intern(name, id, email, school)
-            finalTeamArray.push(teamMember)
-            addTeamMembers()
-    });
-}
+    ]).then((myInput) => {
+      var intern = new Intern (myInput.internName, myInput.internId, myInput.internEmail, myInput.internSchool);
+      myTeamArray.push(intern);
+      startPrompt();
+    })
+};
 
 const print = () => {
 
     let html = generateHTML(myTeamArray);
-        htmlAsync('./dist/index.html', html)
-        .then(() => console.log('Successfully wrote to index.html'))
+        htmlAsync('./_dist_/index.html', html)
+        .then(() => console.log('HTML written, you are good to go'))
         .catch((err) => console.error(err));
   }
   
-
-startPrompt()
-
-const init = () => {
-
-    addMyTeam();
-      
-  };
+  // Runs application
+  const init = () => {
   
-  init();
+    startPrompt();
+        
+    };
+    
+    init();
